4553f290b68b20cd78ad04925feb2fa76051e69d
steve <steveblomeley@yahoo.co.uk>
2018-02-24 23:33:47 +0000

    Implement Lugn card no. check algorithm using altMap

--- Haskell/ProgrammingInHaskell/Chapter_7.hs
+++ Haskell/ProgrammingInHaskell/Chapter_7.hs
@@ -31,33 +31,36 @@ f1thenf2 = foldr (.) id fs
 
 
 -- Ex. 1. express the list comprehension [f x | x <- xs, p x] in terms of map & filter
-filterAndMap :: (a -> Bool) -> (a -> b) -> [a] -> [b]
+type Predicate a = a -> Bool
+type Map a b = a -> b
+
+filterAndMap :: Predicate a -> Map a b -> [a] -> [b]
 filterAndMap p f xs = map f $ filter p xs
 
 
 -- Ex 2. implement a version of each of the following functions from the prelude
 
-all' :: (a -> Bool) -> [a] -> Bool
+all' :: Predicate a -> [a] -> Bool
 all' p xs = foldr (\x a -> (p x) && a) True xs
 
-any' :: (a -> Bool) -> [a] -> Bool
+any' :: Predicate a -> [a] -> Bool
 any' p xs = foldr (\x a -> (p x) || a) False xs
 
-takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' :: Predicate a -> [a] -> [a]
 takeWhile' _ [] = []
 takeWhile' p (x:xs) | p x = x : takeWhile' p xs
                     | otherwise = []
 
-dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' :: Predicate a -> [a] -> [a]
 dropWhile' _ [] = []
 dropWhile' p (x:xs) | p x = dropWhile' p xs
                     | otherwise = x:xs
 
 -- Ex 3. Redefine map f and filter p using foldr
-map' :: (a -> b) -> [a] -> [b]
+map' :: Map a b -> [a] -> [b]
 map' f = foldr (\x acc -> f x : acc) []
 
-filter' :: (a -> Bool) -> [a] -> [a]
+filter' :: Predicate a -> [a] -> [a]
 filter' p = foldr (\x acc -> if p x then x : acc else acc) []
 
 
@@ -80,7 +83,7 @@ uncurry' f = \(x,y) -> f x y
 
 type Bit = Int
 
-unfold :: (a -> Bool) -> (a -> b) -> (a -> a) -> a -> [b]
+unfold :: Predicate a -> (a -> b) -> (a -> a) -> a -> [b]
 unfold p h t x | p x       = []
                | otherwise = h x : unfold p h t (t x)
 
@@ -104,3 +107,14 @@ iterate' f = unfold (\x -> False) f (\x -> x)
 altMap :: (a -> b) -> (a -> b) -> [a] -> [b]
 altMap _ _ [] = []
 altMap f1 f2 (x:xs) = f1 x : altMap f2 f1 xs
+
+
+-- Ex 10. use altMap to implement Luhn card check algorithm
+luhnDouble :: (Integral a) => a -> a
+luhnDouble x =
+    if (doubled > 9) then doubled - 9 else doubled
+    where doubled = 2 * x
+
+luhn :: [Int] -> Bool
+luhn xs = (sum luhned) `mod` 10 == 0
+          where luhned = altMap (\x -> x) (\x -> luhnDouble x) (reverse xs)
