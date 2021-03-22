d950949a6d53560e7443fc6ca63ae31e4ec0215e
steve <steveblomeley@yahoo.co.uk>
2021-03-22 12:35:20 +0000

    Spelling Bee - add comments to describe the game

--- Haskell/NytSpellingBee/SpellingBee.hs
+++ Haskell/NytSpellingBee/SpellingBee.hs
@@ -1,3 +1,39 @@
+-- Command line game based on the New York Times Spelling Bee
+--
+-- Program selects 7 random letters (must include >0 and <4 vowels)
+-- One letter is nominated to be mandatory, i.e. must be present in every word played
+--
+-- The player enters words - to be valid, a word must:
+-- - Contain >4 characters
+-- - Contain at least one occurence of the mandatory letter
+-- - Be comprised entirely from the 7 selected letters
+-- - Be present in a dictionary of standard english words
+-- - Not be a word that the player has already played
+--
+-- Each valid word is scored - 1 point for 4 letter word, then 1 extra point for each extra letter
+--
+-- At start of game, program uses the 7 letters in play to filter its standard dictionary for valid words
+-- It then calculates the maximum possible score (i.e. if a player guessed all of the possible valid words)
+-- After the player plays a valid word, the players score is updated 
+--
+-- After each "play", whether entered word is valid or not, the game displays:
+-- - The 7 letters in play (highlighting the mandatory letter)
+-- - Number of valid words played so far
+-- - Player's score - "Score: <a>/<p> points" <- <a> is actual points, <p> is possible points
+-- - Message indicating if the last word played was valid
+--   - "Good word! x points."
+--   - "You already played that word."
+--   - "Too short - word must be at least 4 letters"
+--   - "Word must contain the letter <m>" <- <m> is the mandatory letter
+--   - "Word can only contain the letters a, b, c, d, e, f & g" <- where abcdefg are the letters in play for this game
+--
+-- The game will include some special commands - probably GHCi style, with ":" prefix
+-- :q(uit)        quit the game - you'll be shown a list of all possible words (nice to have - highlight the ones
+--                you guessed)
+-- :n(ew)         start new game (you'll still be shown list of all possible words before new game starts)
+-- :l(ist)        list all words guessed so far in alphabetical order
+-- :? or ?h(elp)  list all of these commands
+
 type Dictionary = [String] 
 
 dict :: Dictionary
