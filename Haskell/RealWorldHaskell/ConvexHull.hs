703318331a468168cb28b708eb0ecf2fc4bf5261
steve <steveblomeley@yahoo.co.uk>
2021-04-18 14:13:53 +0100

    Fix convex hull - must work back to a left turn after finding a right turn

--- Haskell/RealWorldHaskell/ConvexHull.hs
+++ Haskell/RealWorldHaskell/ConvexHull.hs
@@ -2,6 +2,8 @@ import Data.List
 
 type Point = (Double,Double)
 
+data Direction = LeftTurn | RightTurn deriving Eq
+
 orderBottomUp :: [Point] -> [Point]
 orderBottomUp ps = sortBy (\(x,y) (x',y') -> compare y y') ps
 
@@ -21,9 +23,13 @@ sortFromOrigin (xo,yo) = sortBy (\p p' -> compare (angleFromOrigin p) (angleFrom
                          where
                              angleFromOrigin (x,y) = atan2 (y-yo) (x-xo)
 
-zCoordinateXpOfV :: Point -> Point -> Point -> Double
-zCoordinateXpOfV (x1,y1) (x2,y2) (x3,y3) = 
-    (x2 - x1) * (y3 -y1) - (y2 - y1) * (x3 - x1)               
+directionOfTurn :: Point -> Point -> Point -> Direction
+directionOfTurn (x1,y1) (x2,y2) (x3,y3) = 
+    if zCoordinateOfXProductOfVectors < 0
+        then RightTurn
+        else LeftTurn
+    where
+        zCoordinateOfXProductOfVectors = (x2 - x1) * (y3 -y1) - (y2 - y1) * (x3 - x1)               
 
 sortPointsFromOrigin :: [Point] -> (Point,[Point])
 sortPointsFromOrigin ps = (pOrigin, psSorted ++ [pOrigin])
@@ -34,12 +40,12 @@ sortPointsFromOrigin ps = (pOrigin, psSorted ++ [pOrigin])
 
 convexHullInternal :: [Point] -> [Point] -> [Point]
 convexHullInternal psToKeep [_] = psToKeep
-convexHullInternal psToKeep (pThis:pNext:psRemaining) = 
-    if zCoordinateOfXProductOfVectors >= 0
-        then convexHullInternal (psToKeep ++ [pThis]) (pNext:psRemaining)
-        else convexHullInternal  psToKeep             (pNext:psRemaining)
+convexHullInternal (pLast:psToKeep) (pThis:pNext:psRemaining) = 
+    if direction == RightTurn
+        then convexHullInternal              psToKeep  (pLast:pNext:psRemaining)
+        else convexHullInternal (pThis:pLast:psToKeep)       (pNext:psRemaining)
     where
-        zCoordinateOfXProductOfVectors = zCoordinateXpOfV (last psToKeep) pThis pNext
+        direction = directionOfTurn pLast pThis pNext
 
 convexHull :: [Point] -> [Point]
 convexHull ps
