0b7daf1217c11d6d15d70accf528c850dcc9b456
steve <steveblomeley@yahoo.co.uk>
2021-04-20 08:36:41 +0100

    Convex hull - refactor for legibility

--- Haskell/RealWorldHaskell/ConvexHull.hs
+++ Haskell/RealWorldHaskell/ConvexHull.hs
@@ -4,22 +4,49 @@ type Point = (Double,Double)
 
 data Direction = LeftTurn | RightTurn deriving Eq
 
-orderBottomUp :: [Point] -> [Point]
-orderBottomUp ps = sortBy (\(x,y) (x',y') -> compare y y') ps
+sortByX :: [Point] -> [Point]
+sortByX = sortBy (\(x,_) (x',_) -> compare x x')
 
-orderLeftToRight :: [Point] -> [Point]
-orderLeftToRight = sortBy (\(x,_) (x',_) -> compare x x')
+xValue :: Point -> Double
+xValue (x,_) = x
+
+yValue :: Point -> Double
+yValue (_,y) = y
+
+xValues :: [Point] -> [Double]
+xValues = map xValue
+
+yValues :: [Point] -> [Double]
+yValues = map yValue
+
+xEquals :: Double -> Point -> Bool
+xEquals n = (==) n . xValue
+
+yEquals :: Double -> Point -> Bool
+yEquals n = (==) n . yValue
+
+whereXEquals :: [Point] -> Double -> [Point]
+whereXEquals ps x = filter (xEquals x) ps
+
+whereYEquals :: [Point] -> Double -> [Point]
+whereYEquals ps y = filter (yEquals y) ps
+
+lowest :: Ord a => [a] -> a
+lowest = foldr1 min
 
 leftMostBottomMost :: [Point] -> Point
 leftMostBottomMost ps = 
-    head $ orderLeftToRight bottomPs
+    head $ psWithLowestYValue `whereXEquals` lowestX
     where 
-        bottomPs = takeWhile (\(_,y) -> y == lowestY) bottomToTop
-        (_,lowestY) = head $ bottomToTop
-        bottomToTop = orderBottomUp ps
+        lowestX = lowest $ xValues psWithLowestYValue
+        psWithLowestYValue = ps `whereYEquals` lowestY
+        lowestY = lowest $ yValues ps
 
-sortFromOrigin :: Point -> [Point] -> [Point]
-sortFromOrigin po = sortBy (\p1 p2 -> sorter po p1 p2)
+angleFromOrigin :: Point -> Point -> Double
+angleFromOrigin (xo,yo) (x,y) = atan2 (y-yo) (x-xo)
+
+distanceBetween :: Point -> Point -> Double
+distanceBetween (xo,yo) (x,y) = sqrt( ((x-xo)^2) + ((y-yo)^2) )
 
 sorter :: Point -> Point -> Point -> Ordering
 sorter po p1 p2 = if angle1 /= angle2
@@ -31,11 +58,12 @@ sorter po p1 p2 = if angle1 /= angle2
                       distance1 = distanceBetween po p1
                       distance2 = distanceBetween po p2
                       
-angleFromOrigin :: Point -> Point -> Double
-angleFromOrigin (xo,yo) (x,y) = atan2 (y-yo) (x-xo)
-
-distanceBetween :: Point -> Point -> Double
-distanceBetween (xo,yo) (x,y) = sqrt( ((x-xo)^2) + ((y-yo)^2) )
+sortPointsFromOrigin :: [Point] -> (Point,[Point])
+sortPointsFromOrigin ps = (pOrigin, psSorted ++ [pOrigin])
+                          where
+                              psSorted = sortBy (sorter pOrigin) remainingPs
+                              remainingPs = filter ((/=) pOrigin) ps
+                              pOrigin = leftMostBottomMost ps
 
 directionOfTurn :: Point -> Point -> Point -> Direction
 directionOfTurn (x1,y1) (x2,y2) (x3,y3) = 
@@ -45,25 +73,18 @@ directionOfTurn (x1,y1) (x2,y2) (x3,y3) =
     where
         zCoordinateOfXProductOfVectors = (x2 - x1) * (y3 -y1) - (y2 - y1) * (x3 - x1)               
 
-sortPointsFromOrigin :: [Point] -> (Point,[Point])
-sortPointsFromOrigin ps = (pOrigin, psSorted ++ [pOrigin])
-                          where
-                              pOrigin = leftMostBottomMost ps
-                              remainingPs = filter (\p -> p /= pOrigin) ps
-                              psSorted = sortFromOrigin pOrigin remainingPs
-
-convexHullInternal :: [Point] -> [Point] -> [Point]
-convexHullInternal psToKeep [_] = psToKeep
-convexHullInternal (pLast:psToKeep) (pThis:pNext:psRemaining) = 
+convexHullInternal :: [Point] -> Point -> [Point] -> [Point]
+convexHullInternal psToKeep pOrigin [] = psToKeep
+convexHullInternal (pLast:psToKeep) pThis (pNext:psRemaining) = 
     if direction == RightTurn
-        then convexHullInternal              psToKeep  (pLast:pNext:psRemaining)
-        else convexHullInternal (pThis:pLast:psToKeep)       (pNext:psRemaining)
+        then convexHullInternal psToKeep pLast (pNext:psRemaining)        -- discard and move back
+        else convexHullInternal (pThis:pLast:psToKeep) pNext psRemaining  -- keep and move forward
     where
         direction = directionOfTurn pLast pThis pNext
 
 convexHull :: [Point] -> [Point]
 convexHull ps
     | length ps < 4 = ps
-    | otherwise     = convexHullInternal [pOrigin] sortedPs
+    | otherwise     = convexHullInternal [pOrigin] (head sortedPs) (tail sortedPs)
                       where 
                           (pOrigin, sortedPs) = sortPointsFromOrigin ps
